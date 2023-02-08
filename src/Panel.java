3f4559dad72d9c2f62c74747efe0533364236e45
Maksym <milian.code@gmail.com>
2023-02-08 14:35:28 +0100

    https://github.com/MilianCode/snake-game.git

---
+++ src/Panel.java
@@ -0,0 +1,52 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
+
+public class Panel extends JPanel implements ActionListener {
+
+    Panel(){
+
+    }
+
+    public void startGame(){
+
+    }
+
+    public void paintComponent(Graphics g){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void checkApple(){
+
+    }
+
+    public void checkCollisions(){
+
+    }
+
+    public void gameOver(Graphics g){
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public class MyKeyAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent keyEvent){
+
+        }
+    }
+}
